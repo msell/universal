c789bc3e95c37f848c1a762ece6c595808f3e06e
Matt <matt.sell@beacon.works>
2020-01-24 22:11:25 -0600

    env config

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { getEnvVars } from "config";
 
 export const Widget: React.FC = () => (
   <View style={styles.container}>
     <Text>Widget</Text>
+    <Text>{JSON.stringify(getEnvVars(), null, 2)}</Text>
   </View>
 );
 
